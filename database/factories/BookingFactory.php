9271cd3502fe20608210fd310276c4ba4aa23014
hasensuadik <hasensuadik39@gmail.com>
2022-06-13 23:47:07 +0300

    all are done

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Booking;
+use App\Models\Service;
 use App\Models\User;
 
 class BookingFactory extends Factory
@@ -22,13 +23,13 @@ class BookingFactory extends Factory
     */
     public function definition(): array
     {
+        $service = Service::inRandomOrder()->first();
         return [
             'id' => $this->faker->uuid,
-            'service_id' => \App\Models\Service::factory(),
-            'service_provider_id' => \App\Models\ServiceProvider::factory(),
-            'user_id' => User::inRandomOrder()->first(),
-            'status' => 1,
-            'service_date' => $this->faker->date(),
+            'service_id' => $service->id,
+            'service_provider_id' => $service->service_provider_id,
+            'user_id' => 2,
+            'status' => 2,
         ];
     }
 }
--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Booking;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
 
 class BookingSeeder extends Seeder
 {
@@ -15,6 +18,25 @@ class BookingSeeder extends Seeder
      */
     public function run()
     {
-        Booking::factory(10)->create();
+        $first_date    =   Carbon::now()->subMonths(2);
+        $second_date   =   $first_date->addWeek();
+        $third_date    =   $second_date->addWeek();
+        $fourth_date   =   $third_date->addWeek();
+        $fifth_date    =   $fourth_date->addWeek();
+        $sixth_date    =   $fifth_date->addWeek();
+        $seventh_date  =   $sixth_date->addWeek();
+        $eight_date    =   $seventh_date->addWeek();
+
+
+        Booking::factory(8)->state(new Sequence([
+            ['service_date'=>$first_date],
+            ['service_date'=>$second_date],
+            ['service_date'=>$third_date],
+            ['service_date'=>$fourth_date],
+            ['service_date'=>$fifth_date],
+            ['service_date'=>$sixth_date],
+            ['service_date'=>$seventh_date],
+            ['service_date'=>$eight_date]
+        ]))->create();
     }
 }
