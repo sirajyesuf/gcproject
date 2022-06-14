e2e5ac3657a746e155857296b60723a3bfad22cf
hasensuadik <hasensuadik39@gmail.com>
2022-06-14 07:45:32 +0300

    return

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -19,13 +19,13 @@ class BookingSeeder extends Seeder
     public function run()
     {
         $first_date    =   Carbon::now()->subMonths(2);
-        $second_date   =   $first_date->addWeek();
-        $third_date    =   $second_date->addWeek();
-        $fourth_date   =   $third_date->addWeek();
-        $fifth_date    =   $fourth_date->addWeek();
-        $sixth_date    =   $fifth_date->addWeek();
-        $seventh_date  =   $sixth_date->addWeek();
-        $eight_date    =   $seventh_date->addWeek();
+        $second_date   =   $first_date->copy()->addWeek();
+        $third_date    =   $second_date->copy()->addWeek();
+        $fourth_date   =   $third_date->copy()->addWeek();
+        $fifth_date    =   $fourth_date->copy()->addWeek();
+        $sixth_date    =   $fifth_date->copy()->addWeek();
+        $seventh_date  =   $sixth_date->copy()->addWeek();
+        $eight_date    =   $seventh_date->copy()->addWeek();
 
 
         Booking::factory(8)->state(new Sequence(
